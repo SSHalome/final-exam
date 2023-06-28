1fa0ab1ee2a72516b4816b88e9c8d630cb8955d0
Ubuntu <ubuntu@ip-172-31-82-57.ec2.internal>
2023-06-28 17:33:08 +0000

    initial

--- skripti.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "# final-exam" >> README.md
-git init
-git add README.md
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:SSHalome/final-exam.git
-git push -u origin main
